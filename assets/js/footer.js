6088789ba14966bbbc0d88ee267afdc01ee37b10
Ashkan Y <yousefpour.ashkan@gmail.com>
2019-02-09 09:45:13 -0800

    Update header and footer
    Update header and footer

---
+++ assets/js/footer.js
@@ -0,0 +1,18 @@
+document.write('<footer class="background-1 color-8 py-3 fs--1 fw-600 font-heading">\
+                    <div class="container">\
+                        <div class="row align-items-center">\
+                            <div class="col-auto font-1">&copy; 2019 Mobile Sensing Lab | Developed by <a target="_blank" href="http://www.utdallas.edu/~ashkan/">Ashkan Yousefpour</a></div>\
+                            <div class="col fs-0 text-right">\
+                                <a class="p-2 color-8"  target="_blank" href="https://twitter.com/BerkeleyMsl">\
+                                    <i class="fa fa-twitter"></i>\
+                                </a>\
+                                <a class="p-2 color-8"  target="_blank" href="mailto:flow.berkeley@gmail.com">\
+                                    <i class="fa fa-envelope"></i>\
+                                </a>\
+                                <a class="p-2 color-8"  target="_blank" href="https://groups.google.com/forum/#!forum/flow-dev">\
+                                    <i class="fa fa-google"></i>\
+                                </a>\
+                            </div>\
+                        </div>\
+                    </div>\
+                </footer>');
---
+++ assets/js/header.js
@@ -0,0 +1,50 @@
+document.write('<div class="znav-container znav" id="znav-container">\
+                    <div class="container">\
+                        <nav class="navbar navbar-toggleable-md">\
+                            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">\
+                                <div class="hamburger hamburger--emphatic">\
+                                    <div class="hamburger-box">\
+                                        <div class="hamburger-inner"></div>\
+                                    </div>\
+                                </div>\
+                            </button>\
+                            <a class="navbar-brand overflow-hidden" href="index.html">\
+    <!--                             <span class="icon-Knight fw-300" ></span>-->\
+                                Flow\
+    <!--                            <img src="assets/images/posh-logo.png" alt="logo" width="80px">-->\
+                            </a>\
+                            <div class="collapse navbar-collapse" id="navbarNavDropdown">\
+                                <ul class="navbar-nav ml-auto">\
+                                    <li>\
+                                         <a href="usingflow.html"> <span class="icon-Arrow-Right2 fw-300 color-warning" ></span> Get Started</a>\
+                                    </li>\
+                                    <li>\
+                                        <a href="JavaScript:void(0)">Resources</a>\
+                                        <ul class="dropdown dropdown-push-left">\
+                                            <li>\
+                                                <a href="gallery.html">Gallery</a>\
+                                            </li>\
+                                            <li>\
+                                                <a href="publications.html">Publications</a>\
+                                            </li>\
+                                            <li>\
+                                                <a href="tutorial.html">Tutorials</a>\
+                                            </li>\
+                                        </ul>\
+                                    </li>\
+                                    <li>\
+                                        <a href="JavaScript:void(0)">About</a>\
+                                        <ul class="dropdown">\
+                                            <li>\
+                                                <a href="team.html">Team</a>\
+                                            </li>\
+                                            <li>\
+                                                <a href="contact.html">Contact</a>\
+                                            </li>\
+                                        </ul>\
+                                    </li>\
+                                </ul>\
+                            </div>\
+                        </nav>\
+                    </div>\
+            </div>');
