1aea63976d684683f1ad7b2ff73279d05b3923f8
Ashkan Y <yousefpour.ashkan@gmail.com>
2019-03-19 16:20:18 -0700

    Added flow icon to the favicon

---
+++ assets/js/favicon.js
@@ -0,0 +1,15 @@
+document.write('<link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicons/fav.png">\
+        <link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicons/fav.png">\
+        <link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicons/fav.png">\
+        <link rel="apple-touch-icon" sizes="76x76" href="assets/images/favicons/fav.png">\
+        <link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicons/fav.png">\
+        <link rel="apple-touch-icon" sizes="120x120" href="assets/images/favicons/fav.png">\
+        <link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicons/fav.png">\
+        <link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicons/fav.png">\
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/fav.png">\
+        <link rel="icon" type="image/png" sizes="192x192" href="assets/images/favicons/fav.png">\
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicons/fav.png">\
+        <link rel="icon" type="image/png" sizes="96x96" href="assets/images/favicons/fav.png">\
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicons/fav.png">\
+        <meta name="msapplication-TileColor" content="#ffffff">\
+        <meta name="msapplication-TileImage" content="assets/images/favicons/fav.png">');
