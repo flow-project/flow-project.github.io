ef097f7a2942c8d06fa8f7eab54466eb2f7b8211
Hristo Filaretov <h.filaretov@campus.tu-berlin.de>
2019-12-01 17:05:19 +0100

    fix typo in footer

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,7 +1,7 @@
 document.write('<footer class="background-1 color-8 py-3 fs--1 fw-600 font-heading">\
                     <div class="container">\
                         <div class="row align-items-center">\
-                            <div class="col-auto font-1">&copy; 2019 Mobile Sensing Lab | Wesbite developed by <a target="_blank" href="http://www.utdallas.edu/~ashkan/">Ashkan Yousefpour</a></div>\
+                            <div class="col-auto font-1">&copy; 2019 Mobile Sensing Lab | Website developed by <a target="_blank" href="http://www.utdallas.edu/~ashkan/">Ashkan Yousefpour</a></div>\
                             <div class="col fs-0 text-right">\
                                 <a class="p-2 color-8"  target="_blank" href="https://twitter.com/BerkeleyMsl">\
                                     <i class="fa fa-twitter"></i>\
